0b373b22c4433a75a0ee4c99c87775f0dae982b6
Arnav Dewan <57629464+arnxv0@users.noreply.github.com>
2022-04-03 12:18:47 +0530

    Add basic frontend files

---
+++ frontend/src/components/pages/LangingPage/LandingPage.style.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const LandingPageContainer = styled.div`
+  padding: 20px 10%;
+  background-color: white;
+`;
+
+export const Title = styled.h1`
+  /* color: ${({ theme }) => theme.secondaryFontColor}; */
+  color: black;
+`;
